04e3c9b529300fabe2e887268a9eb69674ccc3f7
dmytro <dima.berezniy.13@gmail.com>
2026-03-30 10:05:07 +0300

    feat: fetcher to parse links draft

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -1,9 +1,13 @@
 package fetcher
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
+
+	"golang.org/x/net/html"
 )
 
 type Fetcher struct {
@@ -24,6 +28,36 @@ func (f *Fetcher) Fetch(u *url.URL) (int, time.Duration, error) {
 		return 0, dur, err
 	}
 	defer resp.Body.Close()
+
+	z := html.NewTokenizer(resp.Body)
+
+loop:
+	for {
+		tt := z.Next()
+
+		switch tt {
+		case html.ErrorToken:
+			if err = z.Err(); err == io.EOF {
+				break loop
+			}
+			return resp.StatusCode, dur, err
+		case html.StartTagToken:
+			tn, _ := z.TagName()
+			if len(tn) == 1 && tn[0] == 'a' {
+				for {
+					k, v, more := z.TagAttr()
+					if string(k) == "href" {
+						fmt.Println(string(v))
+						break
+					}
+					if !more {
+						break
+					}
+				}
+			}
+		default:
+		}
+	}
 
 	return resp.StatusCode, dur, nil
 }
